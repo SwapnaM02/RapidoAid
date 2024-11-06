abda39d15bd2f92322e4bc220a75d1c9ff7af2a6
SwapnaM02 <m.v.swapnareddy@gmail.com>
2024-11-06 08:55:07 +0530

    initial setup

---
+++ client/src/pages/Login/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import { Button, Checkbox, Form, Input } from 'antd';
+
+
+const Login = () => {
+  return (
+    <>
+    <main className='App-header'>
+    <h1> Login to RapidAid</h1>
+    <section className="mw-500  " >
+        <h1>hello</h1>
+    </section>
+
+    </main>
+    </>
+    
+  
+)
+
+  
+}
+
+export default Login
---
+++ client/src/pages/Register/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const index = () => {
+  return (
+    <div>
+      I am register pages
+    </div>
+  )
+}
+
+export default index
